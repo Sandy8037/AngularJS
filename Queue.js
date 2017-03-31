f38fb2822a1326eb7dde8cf1fb90332359f39622
Sandeep Devarapalli <sandy8037@gmail.com>
2017-03-31 14:38:59 -0700

    Queue Data Structure using JavaScript

---
+++ Queue.js
@@ -0,0 +1,26 @@
+(function () {
+'use strict';
+
+var arr = [], min;
+
+for (var i = 0; i < 5; i++) {
+  arr[i] = i + 1;
+}
+
+console.log(arr.shift());
+
+for (var i = 0; i < arr.length; i++) {
+  if (i === 0) {
+    min = arr[i];
+  }
+  else {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+}
+
+console.log(arr);
+console.log(min);
+
+})();
